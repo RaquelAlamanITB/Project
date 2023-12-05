16618266635255a8e20c48dc52beb8c202f95826
prof <prof@MAQUETA>
2023-12-05 12:09:44 +0100

    Add project files.

---
+++ Project/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Projectes {
+    public class Project { 
+        public static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine(num);
+        }
+    }
+}
