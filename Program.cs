11d0bbad642f1035614a9392afb52158bf809b95
“Raquel <raquel.alaman@itb.cat>
2023-12-06 12:57:13 +0100

    Added isValid feature. Closes #1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace MyProjects
+{
+    public class Projecte {
+        public static void Main() {
+            int num;
+            const string Msg = "Introdueix un numero natural (més gran que 0): ";
+            do {
+                Console.WriteLine(Msg);
+                num = Convert.ToInt32(Console.ReadLine());
+            }  while (!IsPositive(num));
+        }
+
+        public static bool IsPositive(int num) { return num > 0; }
+    }
+}
